586125345f1ba8dfed84b41d09813af07bc41f99
ste-obado <stephenomoto27@gmail.com>
2026-06-30 20:49:34 +0300

    grade system

---
+++ transcrip1.py
@@ -0,0 +1,19 @@
+#student transcript
+print("=========================================")
+   
+mark = int(input("Enter mark for unit :"))
+if mark <=39:
+   print("Grade: F")
+elif mark <=49:
+   print("Grade: D")
+elif mark <=59:
+   print("Grade: C")
+elif mark <=69:
+   print("Grade: B")
+else:
+   print("Grade: A")
+
+
+
+
+    
